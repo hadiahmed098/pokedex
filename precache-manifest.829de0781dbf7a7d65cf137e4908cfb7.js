fc39dc79d16b1b8052bff5139eea5c050e356916
Hadi Ahmed <hadiahmed098@gmail.com>
2020-10-17 02:21:48 -0400

    Updates

---
+++ precache-manifest.829de0781dbf7a7d65cf137e4908cfb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8e29998d34addaedce36cacfd153a8",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "aa98b0600784e32c579f",
+    "url": "/pokedex/static/css/main.0d0c39d7.chunk.css"
+  },
+  {
+    "revision": "9d972eb878a8a5c791d8",
+    "url": "/pokedex/static/js/2.a6fa2e9e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pokedex/static/js/2.a6fa2e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa98b0600784e32c579f",
+    "url": "/pokedex/static/js/main.e4c77249.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
