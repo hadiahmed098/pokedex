8a93354f7f307af0e70812bd81bbb04cb3313faf
Hadi Ahmed <hadiahmed098@gmail.com>
2020-11-28 03:04:09 -0600

    Multipage test failed, reverting back to single page setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
 import Header from './component/Header';
 import PokeTable from './component/PokeTable';
 import Search from './component/Search';
-import SingePage from './component/SinglePage';
-import Default from './component/Default';
 
 function App() {
   return (
     // Type your website layout here...
     <div id="app">
       <Header />
-      <Switch>
-        <Route exact path='/' component={PokeTable} />
-        <Route path='/details' component={SingePage} />
-        <Route component={Default} />
-      </Switch>
+      <PokeTable />
       <hr />
       <Search />
     </div>
