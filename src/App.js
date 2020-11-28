116b0c2c560541690017daa44a3c025801dd191b
Hadi Ahmed <hadiahmed098@gmail.com>
2020-11-28 02:46:15 -0600

    Testing a multipage setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import Header from './component/Header';
 import PokeTable from './component/PokeTable';
 import Search from './component/Search';
+import SingePage from './component/SinglePage';
+import Default from './component/Default';
 
 function App() {
   return (
     // Type your website layout here...
     <div id="app">
       <Header />
-      <PokeTable />
+      <Switch>
+        <Route exact path='/' component={PokeTable} />
+        <Route path='/details' component={SingePage} />
+        <Route component={Default} />
+      </Switch>
       <hr />
       <Search />
     </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
     render () {
         const header = 
         <div className="header">
-            <h1>PokeDex Pokemon Viewer</h1>
+            <h1><a className="header-link" href="/">PokeDex Pokemon Viewer</a></h1>
             <h3>Developed during Software Saturdays Fall 2020</h3>
             <h3>For more information, visit <a href="https://purdueieee.org/software">our website</a>.</h3>
             <hr />
